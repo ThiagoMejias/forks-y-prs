03c8edde223c2da45dbc2656e11237fd4ddfcf01
unknown <mejiasthiago1@gmail.com>
2021-05-19 15:10:17 -0300

    problemas arreglados

--- index.ts
+++ index.ts
@@ -8,6 +8,11 @@ class Product {
   id: number;
   name: string;
   price: number;
+
+  static findProductsBelow(precioBase : number){
+    return products.filter(products => products.price < precioBase)
+  }
+  
 }
 
 class User {
@@ -20,7 +25,7 @@ class User {
     this.products.push(newProduct);
   }
   addProducts(newProducts: Product[]) {
-    this.products.push(newProducts);
+    this.products =this.products.concat(newProducts);
   }
 }
 
